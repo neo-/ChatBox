427eb3285582339efe8cd86b11f4e6a7e40423ac
RAJEEVAN <naveejr@gmail.com>
2025-03-28 12:35:42 +0530

    Update main.py

--- Server/main.py
+++ Server/main.py
@@ -16,7 +16,7 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 users = {
-    "rajeevan": generate_password_hash("865865")
+    "rajeevan": generate_password_hash("121212")
 }
 
 
